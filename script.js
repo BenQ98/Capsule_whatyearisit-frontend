74047e57ac4909775f448bfa9f99d9bb97e9c406
Benoit <bquellec@d2a.mc>
2025-10-27 20:14:41 +0100

    url change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Hello ...")
 
-fetch("https://capsule-whatyearisit-backend.vercel.app/date")
+fetch("https://capsule-whatyearisit-backend.vercel.app/year")
     .then(response => response.json())
     .then(data => {
         document.querySelector("h1").textContent = data.year;
